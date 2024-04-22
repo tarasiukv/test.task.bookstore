ec66fcc4151d5ef5be7b1709d5d1273858e3fe71
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-04-22 17:33:51 +0300

    Add: semantic tags head, header and footer

---
+++ view/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?> - Bookstore</title>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
+</head>
+<body>
---
+++ view/header.php
@@ -0,0 +1,15 @@
+<?php
+include 'add_book.php'
+?>
+<header class="header">
+    <div class="container">
+        <h1> BOOKSTORE </h1>
+        <nav class="header__menu">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/view/catalog.php">Catalog</a></li>
+                <li><a><button id="addBookButton" class="site-btn">Add Book</button></a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
